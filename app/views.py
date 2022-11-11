86d7107014537c9640ebca39404f5cbfd992a40f
GlitchVsc <tolakosb312@gmail.com>
2022-11-11 23:31:35 +0200

    second migration

--- app/views.py
+++ app/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def homepage(request):
     name="tolhs"
     age=23
 
+    user = CustomUser.objects.get(id=1)
 
 
 
     context={
         "onoma": name,
         "etwn": age,
+        "user": user,
     }
     return render(request, "app/index.html", context)
